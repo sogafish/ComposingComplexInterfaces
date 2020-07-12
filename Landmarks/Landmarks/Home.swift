8e4830a9a08a381e3163f4a890bfce369014b1ea
sogafish <k.sogafish@gmail.com>
2020-07-12 23:26:31 +0900

    S5 Add Navigation Between Sections

--- Landmarks/Landmarks/Home.swift
+++ Landmarks/Landmarks/Home.swift
@@ -15,16 +15,53 @@ struct CategoryHome: View {
             by: { $0.category.rawValue }
         )
     }
+    
+    var featured: [Landmark] {
+        landmarkData.filter{ $0.isFeatured }
+    }
+    
+    @State var showingProfile = false
+    
+    var profileButton: some View {
+        Button(action: { self.showingProfile.toggle() }) {
+            Image(systemName: "person.crop.circle")
+                .imageScale(.large)
+                .accessibility(label: Text("User Profile"))
+                .padding()
+        }
+    }
 
     var body: some View {
         NavigationView {
             List {
+                FeaturedLandmarks(landmarks: featured)
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+
                 ForEach(categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: self.categories[key]!)
                 }
+                .listRowInsets(EdgeInsets())
+                
+                NavigationLink(destination: LandmarkList()) {
+                    Text("See All")
+                }
             }
-                .navigationBarTitle(Text("Featured"))
+            .navigationBarTitle(Text("Featured"))
+            .navigationBarItems(trailing: profileButton)
+            .sheet(isPresented: $showingProfile) {
+                Text("UserProfile")
+            }
         }
+    }
+}
+
+struct FeaturedLandmarks: View {
+    var landmarks: [Landmark]
+    var body: some View {
+        landmarks[0].image.resizable()
     }
 }
 
