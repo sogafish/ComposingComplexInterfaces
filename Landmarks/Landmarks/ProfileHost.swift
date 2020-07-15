d1bf97ddd46af2734ba35e020eb8c0a15d2ba083
sogafish <k.sogafish@gmail.com>
2020-07-15 23:37:46 +0900

    324 Delay Edit Propagation

--- Landmarks/Landmarks/ProfileHost.swift
+++ Landmarks/Landmarks/ProfileHost.swift
@@ -10,18 +10,31 @@ import SwiftUI
 
 struct ProfileHost: View {
     @Environment(\.editMode) var mode
-    @EnvironmentObject var userDat: UserData
+    @EnvironmentObject var userData: UserData
     @State var draftProfile = Profile.default
 
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             HStack() {
+                if self.mode?.wrappedValue == .active {
+                    Button("Cancel") {
+                        self.draftProfile = self.userData.profile
+                        self.mode?.animation().wrappedValue = .inactive
+                    }
+                }
+
                 Spacer()
                 
                 EditButton()
             }
             if self.mode?.wrappedValue == .inactive {
                 ProfileSummary(profile: draftProfile)
+                    .onAppear {
+                        self.draftProfile = self.userData.profile
+                    }
+                    .onDisappear {
+                        self.userData.profile = self.draftProfile
+                    }
             } else {
                 ProfileEditor(profile: $draftProfile)
             }
